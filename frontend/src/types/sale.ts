f130c04f06d387ef16a8293fe15d2a5521586bb6
Francisco Correa <franciscocor.neto@gmail.com>
2021-09-11 18:45:03 -0300

    Barchart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,9 @@
 export type SaleSum = {
-    sellerName: string,
-    sum: number
+    sellerName: string;
+    sum: number;
+}
+export type SaleSucess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
